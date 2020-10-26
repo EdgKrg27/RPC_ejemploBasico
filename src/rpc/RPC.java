daecc3b72298571f0e2f8f693b2125bf1ee50503
EdgKrg27 <edgargonzalezsantos7@gmail.com>
2020-10-26 10:44:40 -0600

    first commit

---
+++ src/rpc/RPC.java
@@ -0,0 +1,28 @@
+package rpc;
+
+/**
+ *
+ * @author Edgar Gonzalez Santos al208305804@azc.uam.mx
+ */
+import org.apache.xmlrpc.WebServer;
+
+public class RPC {
+
+	public static void main(String[] args) {
+		try {
+
+			System.out.println("Iniciando el servidor XML-RPC...");
+
+			WebServer server = new WebServer(80);
+			OperacionMatematica op = new OperacionMatematica();
+			server.addHandler("miServidor", op);
+
+			server.start();
+
+			System.out.println("Inicio exitoso del Servidor, queda en espera de peticiones del cliente...");
+
+		} catch (Exception exception) {
+			System.err.println("Server: " + exception);
+		}
+	}
+}
